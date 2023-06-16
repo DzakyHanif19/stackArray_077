4d74a8aef398d673758edc56a48208961d9d13dd
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-06-16 18:06:07 +0700

    menampilkan push pop display dan exit

--- Stackusingarray/Stackusingarray.cpp
+++ Stackusingarray/Stackusingarray.cpp
@@ -55,5 +55,16 @@ public:
 int main() {
 	stackarray s;
 	char ch;
-	
+	while (true) {
+		cout << endl;
+		cout << "\n***Stack Menu***\n";
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Display\n";
+		cout << "4. Exit\n";
+		cout << "\nEnter your choice: ";
+		cin >> ch;
+		switch (ch) {
+
+	}
 }
