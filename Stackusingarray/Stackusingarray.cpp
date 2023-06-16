ebf449d6580b5106589b4544faef61333eb31456
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-06-16 14:57:39 +0700

    membuat class stack array

--- Stackusingarray/Stackusingarray.cpp
+++ Stackusingarray/Stackusingarray.cpp
@@ -3,20 +3,5 @@
 using namespace std;
 
 class stackarray {
-private:
-	int stack_array[5];
-	int top;
 
-public:
-	//constructor
-	stackarray() {
-		top = -1;
-	}
-
-	int push(int element) {
-		if (top == 4) { //step 1
-			cout << "number of data exceeds the limit." << endl;
-			return 0;
-		}
-	}
 };
