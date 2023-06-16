bd1a9f98d9a346c2e1b255d0f4b30ac8b8e6db7e
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-06-16 18:10:09 +0700

    menambahkan case 2

--- Stackusingarray/Stackusingarray.cpp
+++ Stackusingarray/Stackusingarray.cpp
@@ -72,6 +72,14 @@ int main() {
 			s.push(element);
 			break;
 		}
+		case '2': 
+			if (s.empty()) {
+				cout << "\nStack is empty." << endl;
+				break;
+			}
+			s.pop();
+			break;
 
+		}
 	}
 }
